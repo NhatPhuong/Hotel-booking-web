48c6c786dcafb3df747a283f8bf13ca01bd8782c
NhatPhuong <huynhkimnhatphuongn@gmail.com>
2026-02-05 21:42:46 +0700

    Fix ALLOWED_HOSTS and update project framework

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+# Mẫu
+from .models import RoomType
+
+admin.site.register(RoomType)
--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# Mẫu
+class RoomType(models.Model):
+    name = models.CharField(max_length=100) # Ví dụ: Kingsize, Standard
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    total_rooms = models.IntegerField(default=1)
+    available_rooms = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
