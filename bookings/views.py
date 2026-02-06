2e21a84bd53de6bf5016bb1f7b43c5da1b202cbf
KENZY <117808753+NhatPhuong@users.noreply.github.com>
2026-02-06 10:17:10 +0700

    Update views.py

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Mẫu
+from django.shortcuts import render
+from .models import RoomType
+
+def index(request):
+    rooms = RoomType.objects.all()
+    return render(request, 'bookings/index.html', {'rooms': rooms})
